41c14441e2b7709e5641217ff47918d56a63c7ab
UmerKhan1998 <devUk9298@gmail.com>
2025-12-05 11:39:25 +0500

    worked on graphQl config in my nestjs project

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,9 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,6 +19,12 @@ import { AuthModule } from './auth/auth.module';
       envFilePath: '.env', // optional (default is .env)
     }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+
     DatabaseModule,
     ThrottlerModule.forRoot([
       {
@@ -31,6 +40,7 @@ import { AuthModule } from './auth/auth.module';
     ]),
     AuthModule,
     MyLoggerModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [
--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     origin: ['http://localhost:3000'], // your frontend URL
     credentials: true, // allow cookies
   });
+
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3005);
 }
