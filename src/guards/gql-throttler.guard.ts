bd1298b09f2c262be126ff1ef0cbc1e995e9276a
UmerKhan1998 <devUk9298@gmail.com>
2025-12-05 12:52:41 +0500

    push

--- src/guards/gql-throttler.guard.ts
+++ src/guards/gql-throttler.guard.ts
@@ -1,22 +1,13 @@
-// src/guards/gql-throttler.guard.ts
 import { ExecutionContext, Injectable } from '@nestjs/common';
-import { ThrottlerGuard } from '@nestjs/throttler';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
-  protected getRequest(context: ExecutionContext) {
-    const ctxType = context.getType<'http' | 'graphql'>();
-
-    if (ctxType === 'graphql') {
-      const gqlCtx = GqlExecutionContext.create(context);
-      const req = gqlCtx.getContext().req;
-      if (!req) {
-        throw new Error('GraphQL context does not contain req object!');
-      }
-      return req;
-    }
-
-    return context.switchToHttp().getRequest();
+  getRequestResponse(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    const req = ctx.getContext().req;
+    const res = ctx.getContext().res;
+    return { req, res };
   }
 }
